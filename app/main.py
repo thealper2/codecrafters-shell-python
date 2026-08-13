c0a74e9b0d3ee517a5c02ebff09bcb0185c0fd43
Alper Karaca <thisiseternity12@gmail.com>
2026-08-13 22:14:58 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 
+COMMANDS = ["type", "exit", "echo"]
 
 def main():
     while True:
@@ -10,6 +11,11 @@ def main():
             break
         elif command.startswith("echo "):
             print(command[5:])
+        elif command.startswith("type "):
+            if command[5:] in COMMANDS:
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
         else:
            print(f"{command}: command not found")
 
