4271df5eb5e7709797472ab472d47cd4e823f2dc
Alper Karaca <thisiseternity12@gmail.com>
2026-08-14 22:39:09 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -105,6 +105,7 @@ def completer(text, state):
 
 readline.set_completer(completer)
 readline.parse_and_bind("tab: complete")
+readline.set_completer_delims(" \t\n")
 
 def find_in_path(command):
     """Search for an executable command in PATH directories."""
